62549bd559ee44c86fbeadde9b6e3edea76e9021
chervol <chervol@quintagroup.com>
2010-09-08 15:35:38 +0000

    fixed documentation references

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(name='quintagroup.dropdownmenu',
       version=version,
       description="Multilevel portal dropdown menu for Plone sites.",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read() +
-                       open(os.path.join("quintagroup", "dropdownmenu", "menu.txt")).read(),
+                       open(os.path.join("docs", "HISTORY.txt")).read()
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
