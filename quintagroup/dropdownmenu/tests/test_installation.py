cf312903b001939cfcf9e23709589b27aad95048
mylan <mylan@quintagroup.com>
2010-10-15 13:27:43 +0000

    #232: Added uninstallation tests

--- quintagroup/dropdownmenu/tests/test_installation.py
+++ quintagroup/dropdownmenu/tests/test_installation.py
@@ -12,6 +12,7 @@ from base import *
 CSS_RESOURCES = ["++resource++drop_down.css",]
 DEPENDENCIES = ["plone.app.registry",]
 CONFIGLETS = ["dropdownmenu",]
+#REGISTRY_INTERFACE = "quintagroup.dropdownmenu.interfaces.IDropDownMenuSettings"
 
 class TestInstallation(TestCase):
 
@@ -49,9 +50,40 @@ class TestInstallation(TestCase):
         self.assertTrue(registry.records.get(IPREFIX+afield, None),
             "Not registered '%s' registry interface" % IPREFIX)
 
+class TestUninstallation(TestCaseUnInstalled):
+
+    def testUninstalled(self):
+        qi = self.portal.portal_quickinstaller
+        self.assertFalse(qi.isProductInstalled(PROJECT_NAME),
+            '%s not uninstalled.' % PROJECT_NAME)
+
+    def testStyles(self):
+        """ Test styles registration."""
+        cssreg = getToolByName(self.portal, "portal_css")
+        for res in CSS_RESOURCES:
+            self.assertEqual(cssreg.getResource(res), None)
+
+    def testConfiglet(self):
+        cp = self.portal.portal_controlpanel
+        configlets = [ai['id'] for ai in cp.listActionInfos(check_permissions=0)]
+        for cid in CONFIGLETS:
+            self.assertFalse([1 for ai in configlets if ai==cid], 
+                '"%s" configlet not uninstalled from plone control panel' % cid)
+
+    def testBrowserLayer(self):
+        self.assertFalse(IDropDownMenuLayer in blutils.registered_layers(),
+                     "Not unregistered 'IDropDownMenuLayer' browser layer")
+
+    def testRegistry(self):
+        registry = queryUtility(IRegistry)
+        afield = "show_content_tabs"
+        self.assertFalse(registry.records.get(IPREFIX+afield, None),
+            "Not unregistered '%s' registry interface" % IPREFIX)
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
     suite.addTest(makeSuite(TestInstallation))
+    suite.addTest(makeSuite(TestUninstallation))
     return suite
